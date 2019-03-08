39dbe643bc8a6e88079c1019933108f701fadaf7
Philipp Muens <raute1337@gmx.de>
2019-03-08 17:39:50 +0100

    Add Bot vs. Bot program

---
+++ bot_v_bot.py
@@ -0,0 +1,23 @@
+from dlgo import agent
+from dlgo import goboard
+from dlgo import gotypes
+from dlgo.utils import print_board, print_move
+import time
+
+def main():
+    board_size = 9
+    game = goboard.GameState.new_game(board_size)
+    bots = {
+        gotypes.Player.black: agent.naive.RandomBot(),
+        gotypes.Player.white: agent.naive.RandomBot()
+    }
+    while not game.is_over():
+        time.sleep(0.3)
+        print(chr(27) + '[2J')
+        print_board(game.board)
+        bot_move = bots[game.next_player].select_move(game)
+        print_move(game.next_player, bot_move)
+        game = game.apply_move(bot_move)
+
+if __name__ == '__main__':
+    main()
