12cf3e7635a94493044637f6998178d2c9006f52
Philipp Muens <raute1337@gmx.de>
2019-03-08 17:37:19 +0100

    Add Nix setup

---
+++ shell.nix
@@ -0,0 +1,12 @@
+with import <nixpkgs> {};
+stdenv.mkDerivation rec {
+  name = "dev";
+  env = buildEnv {
+    name = name;
+    paths = buildInputs;
+  };
+  buildInputs = [
+    python36
+    python36Packages.six
+  ];
+}
