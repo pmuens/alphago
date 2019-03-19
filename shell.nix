eca6c44944853fcbc75cd5f8c303d4d040ea9986
Philipp Muens <raute1337@gmx.de>
2019-03-19 11:39:23 +0100

    Add new packages to Nix config

--- shell.nix
+++ shell.nix
@@ -6,11 +6,14 @@ stdenv.mkDerivation rec {
     paths = buildInputs;
   };
   buildInputs = [
+    gnugo
     python36
     python36Packages.six
     python36Packages.numpy
     python36Packages.matplotlib
     python36Packages.Keras
     python36Packages.tensorflow
+    python36Packages.h5py
+    python36Packages.flask
   ];
 }
