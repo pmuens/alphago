1381c34381255107c2fda7827acc82010d7bc4c5
Philipp Muens <raute1337@gmx.de>
2019-03-20 18:45:07 +0100

    Add train method to PolicyAgent

--- dlgo/agent/pg.py
+++ dlgo/agent/pg.py
@@ -1,10 +1,20 @@
 import numpy as np
+from keras.optimizers import SGD
 
 from dlgo.agent.base import Agent
 from dlgo.agent.helpers import is_point_an_eye
 from dlgo import encoders
 from dlgo import goboard
 from dlgo import kerasutil
+
+def prepare_experience_data(experience, board_width, board_height):
+    experience_size = experience.actions.shape[0]
+    target_vectors = np.zeros((experience_size, board_width * board_height))
+    for i in range(experience_size):
+        action = experience.actions[i]
+        reward = experience.rewards[i]
+        target_vectors[i][action] = reward
+    return target_vectors
 
 class PolicyAgent(Agent):
     def __init__(self, model, encoder):
@@ -50,6 +60,21 @@ class PolicyAgent(Agent):
         h5file['encoder'].attrs['board_height'] = self._encoder.board_height
         h5file.create_group('model')
         kerasutil.save_model_to_hdf5_group(self._model, h5file['model'])
+
+    def train(self, experience, lr, clipnorm, batch_size):
+        self._model.compile(
+            loss='categorical_crossentropy',
+            optimizer=SGD(lr=lr, clipnorm=clipnorm))
+
+        # includes tensor with board state where the chosen move is the
+        # reward (1 / -1) and the rest is 0
+        target_vectors = prepare_experience_data(
+            experience,
+            self._encoder.board_width,
+            self._encoder.board_height)
+
+        self._model.fit(
+            experience.states, target_vectors, batch_size=batch_size, epochs=1)
 
 def load_policy_agent(h5file):
     model = kerasutil.load_model_from_hdf5_group(h5file['model'])
