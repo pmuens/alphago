ad09b3f79e2823abb10590e9da5507a203645e36
Philipp Muens <raute1337@gmx.de>
2019-03-24 20:06:46 +0100

    Add AlphaGo MCTS play script

---
+++ alphago_mcts_play.py
@@ -0,0 +1,10 @@
+import h5py
+
+from dlgo.agent import load_prediction_agent, load_policy_agent, AlphaGoMCTS
+from dlgo.rl import load_value_agent
+
+fast_policy = load_prediction_agent(h5py.File('alphago_sl_policy.h5', 'r'))
+strong_policy = load_prediction_agent(h5py.File('alphago_rl_policy.h5', 'r'))
+value = load_value_agent(h5py.File('alphago_value.h5', 'r'))
+
+alphago = AlphaGoMCTS(strong_policy, fast_policy, value)
